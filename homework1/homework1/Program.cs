9c5b5a322ca0e08718ee7227e4d44e69c9220b53
健涛 黄 <1754516250@qq.com>
2018-09-14 01:01:22 +0800

    first commit

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数字1：");
+            double number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入数字2：");
+            double number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("结果：" +"\n"+ number1 * number2);
+        }
+    }
+}
---
+++ homework1/topic5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace topic5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int number1, number2, result;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            number1 = Convert.ToInt32(textBox1.Text.ToString());
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            number2 = Convert.ToInt32(textBox2.Text.ToString());
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result = number1 * number2;
+            textBox3.Text = (result.ToString());
+
+        }
+    }
+}
